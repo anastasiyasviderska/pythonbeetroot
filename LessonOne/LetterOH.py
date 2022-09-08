fde3a947e3f532d7a9266a5c86d6a8336d9345f5
Anastasiya Sviderska <aj@gumgumlab.com>
2022-09-08 14:59:57 +0200

    Added LessonOne Homework

---
+++ LessonOne/LetterOH.py
@@ -0,0 +1,13 @@
+space = "#\t#"
+fill = "#########"
+#print(f"{fill}\n{space}\n{space}\n{space}\n{fill}")
+#print("\n") 
+#print (f"{space}\n{space}\n{fill}\n{space}\n{space}")
+
+h = [space, space, fill, space, space]
+for i in h:
+    print(i)
+print("\n") 
+o = [fill, space, space, space, fill]
+for i in o:
+    print (i)
