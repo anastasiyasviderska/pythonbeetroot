198a4c645cea8d01f9b87f226631742f62082c19
Anastasiya Sviderska <aj@gumgumlab.com>
2022-09-09 22:17:53 +0200

    Lesson Two homework

---
+++ LessonTwo/homework.py
@@ -0,0 +1,20 @@
+# task one
+name = "Anastasiya Sviderska"
+date = "September 9, 2022"
+print (f"Good day {name}! {date} is a perfect day to learn some python.")
+
+# task two
+firstname = "Anastasiya"
+lastname = "Sviderska"
+print ("Hello " + firstname + " " + lastname)
+
+# task three
+a = 3
+b = 5
+print (f"{a} + {b} = {a + b}")
+print (f"{a} - {b} = {a - b}")
+print (f"{a} / {b} = {a / b}")
+print (f"{a} * {b} = {a * b}")
+print (f"{a} ** {b} = {a ** b}")
+print (f"{a} % {b} = {a % b}")
+print (f"{a} // {b} = {a // b}")
