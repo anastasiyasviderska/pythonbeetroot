27354ea50bab630fe781b07591ae271aa33edd9e
Anastasiya Sviderska <aj@gumgumlab.com>
2022-09-13 13:01:25 +0200

    Added task solution for mixing lettters from true strings

--- LessonTwo/practice2.py
+++ LessonTwo/practice2.py
@@ -10,6 +10,12 @@ print("".join([partition_var2[0],"don't ", partition_var1[1],partition_var2[2], 
 quantity_cats= new1_cats.casefold().count("cats")
 quantity_dogs=new1_dogs.casefold().count("dogs")
 print(f"Word dogs exist in this two sentences: {quantity_dogs} times. Word cats exist in this two sentences: {quantity_cats}" )
+# count the number of cats and dogs in both sentences at a time
 lower_var = (dogs + cats).lower()
 print (f"The number of dogs is {lower_var.count('dogs')}")
 print (f"The number of cats is {lower_var.count('cats')}")
+
+# Mix the letters in both sentences according to order solution found on stackoverflow
+# https://stackoverflow.com/questions/48403767/merge-two-strings-with-alternate-chars-as-output
+
+print ("".join(map("".join, zip(dogs,cats))))
