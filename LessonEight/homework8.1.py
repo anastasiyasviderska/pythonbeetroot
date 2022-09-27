f221b667523d0b1e472b4e0408feeae76d8a0340
Anastasiya Sviderska <aj@gumgumlab.com>
2022-09-27 14:24:30 +0200

    Lesson Eight homework one

---
+++ LessonEight/homework8.1.py
@@ -0,0 +1,12 @@
+#if the second exception is not implemented then program crashes. 
+# To resolve the issue we can use except without parameters.
+def oops():
+    raise KeyError
+def index_error():
+    try:
+        oops()
+    except IndexError:
+        print("Some Error")
+    except:
+        print ("Other Error")
+index_error()
