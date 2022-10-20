7f5c3f4e617caf391cc7c0e3995af9a009279be4
Anastasiya Sviderska <aj@gumgumlab.com>
2022-10-20 17:35:56 +0200

    homework 13.4

---
+++ Lesson_13/homework13.4.py
@@ -0,0 +1,9 @@
+class CustomException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        with open ('logs.txt', 'a') as file_object:
+            file_object.write(msg)
+            file_object.close()
+
+raise CustomException('Error 1')
+
